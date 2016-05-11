feb8935a43f4a8b4ea3d4819e0000049e4a64bf5
jkhaled <khaled.hadj.a@gmail.com>
2016-05-11 12:11:36 +0100

    update

--- public/index.php
+++ public/index.php
@@ -8,14 +8,38 @@
 
 require '../vendor/autoload.php';
 
+
+
+throw new \ResponseCompressor\Exception\FileNotExistException("file.css");
+
+/*
+
 $content = file_get_contents('../data/page.html');
 
-$compressor = new \ResponseCompressor\Compressor();
+$config = [
+    'base_path'=>__DIR__,
+];
+
+
+$html = '<html><head><link href="/css/style.css" /></head><body></body></html>';
+$config = ['base_path'=>'/var/www/html/psr7-response-minfied/public'];
+
+$compressor = new \ResponseCompressor\Compressor($config);
+$compressor->loadPage($content);
+$compressor->extractCSSFiles();
+
+$fname = $compressor->getCannonicalName('css');
+file_put_contents(__DIR__.DIRECTORY_SEPARATOR.$fname, $compressor->getFinalFileContent('css'));
+
+var_dump($compressor->getFiles());
+/*
+$compressor = new \ResponseCompressor\Compressor($config);
 
 $compressor->loadPage($content);
 $compressor->extractCSSFiles();
-$compressor->extractJSFiles();
+//$compressor->extractJSFiles();
 
 var_dump($compressor->getFiles());
 
 echo $content;
+*/
